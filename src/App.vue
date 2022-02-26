eed52191a18d6772a59df508c341a7852588375c
h3x1e <kingz.williams@gmail.com>
2022-02-26 09:57:10 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="text-h6">
+                        Todo
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        Get things done!
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list dense nav>
+                <v-list-item
+                    v-for="item in items"
+                    :key="item.title"
+                    :to="item.to"
+                    link
+                >
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app color="primary" dark prominent src="/notepad.jpg">
+            <template v-slot:img="{ props }">
+                <v-img
+                    v-bind="props"
+                    gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+                ></v-img>
+            </template>
+
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+            <v-app-bar-title>Todo</v-app-bar-title>
+
+            <v-spacer></v-spacer>
+        </v-app-bar>
+
+        <v-main>
+            <router-view></router-view>
+        </v-main>
+    </v-app>
+</template>
+
+<style>
+.v-app-bar-title__content {
+    width: 100%;
+}
+</style>
+
+<script>
+export default {
+    data: () => ({
+        drawer: null,
+        items: [
+            { title: "Todo", icon: "mdi-list-status", to: "/" },
+            { title: "About", icon: "mdi-help-box", to: "/about" },
+        ],
+    }),
+};
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="about pa-10">
+        <h1>About this Todo List</h1>
+        <p>
+            Curabitur aliquet quam id dui posuere blandit. Nulla quis lorem ut
+            libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet
+            et, porttitor at sem. Cras ultricies ligula sed magna dictum porta.
+            Praesent sapien massa, convallis a pellentesque nec, egestas non
+            nisi. Vestibulum ante ipsum primis in faucibus orci luctus et
+            ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet
+            aliquam vel, ullamcorper sit amet ligula. Sed porttitor lectus nibh.
+            Donec sollicitudin molestie malesuada.
+        </p>
+        <p>
+            Curabitur aliquet quam id dui posuere blandit. Nulla quis lorem ut
+            libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet
+            et, porttitor at sem. Cras ultricies ligula sed magna dictum porta.
+            Praesent sapien massa, convallis a pellentesque nec, egestas non
+            nisi. Vestibulum ante ipsum primis in faucibus orci luctus et
+            ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet
+            aliquam vel, ullamcorper sit amet ligula. Sed porttitor lectus nibh.
+            Donec sollicitudin molestie malesuada.
+        </p>
+        <p>
+            Curabitur aliquet quam id dui posuere blandit. Nulla quis lorem ut
+            libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet
+            et, porttitor at sem. Cras ultricies ligula sed magna dictum porta.
+            Praesent sapien massa, convallis a pellentesque nec, egestas non
+            nisi. Vestibulum ante ipsum primis in faucibus orci luctus et
+            ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet
+            aliquam vel, ullamcorper sit amet ligula. Sed porttitor lectus nibh.
+            Donec sollicitudin molestie malesuada.
+        </p>
+    </div>
+</template>
